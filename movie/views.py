f3ad0bdd1afbbec48ebfc0ae1536ed4d3a8f7237
Diego <damunerat@eafit.edu.co>
2023-02-09 11:29:38 -0500

    Cambios al estilo

--- movie/views.py
+++ movie/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {'name':'Diego Múnera'})
+
+def about(request):
+    return render(request, 'about.html')
